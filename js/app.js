aaa9bca25672d941356640107ebe2bc7df06e874
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-08-10 13:43:32 -0400

    added pseudocode for enemy and player class

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    // what is the x position
+    // what is the y position
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // if the enemy is not at the end
+        // Move forward
+        // Increment x coorinate by speed * dt
+    // else
+        // Reset the enemy position to the beginning
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +35,33 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// class Player
+    // Constructor
+        // x postion
+        // y position
+        // sprite character
+
+    // render()
+        // Draw the player sprite on the right x and y coordinate
+
+    // update()
+        // Update postion
+        // Check for collision
+            // Was there a collison? (Bool)
+        // Check for winning the game
+            // Did the player reach the last tile?
+
+    // handleInput()
+
+    // resetHero()
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+// New Player object
+// Init allEnemies array
+// Player object into player
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
