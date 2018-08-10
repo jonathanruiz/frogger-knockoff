cfd3b370b869fa420fe73f4079246950565d9628
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-08-10 14:44:01 -0400

    made code prettier

--- js/app.js
+++ js/app.js
@@ -1,87 +1,82 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor() {
-        this.sprite = 'images/enemy-bug.png';
-        this.x = 0;
-        this.y = 0;
-    }
+  constructor() {
+    this.sprite = "images/enemy-bug.png";
+    this.x = 0;
+    this.y = 0;
+  }
 
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
-    update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    // if the enemy is not at the end
+    // Move forward
+    // Increment x coorinate by speed * dt
+    // else
+    // Reset the enemy position to the beginning
+  }
 
-        // if the enemy is not at the end
-        // Move forward
-        // Increment x coorinate by speed * dt
-        // else
-        // Reset the enemy position to the beginning
-
-    }
-
-    // Draw the enemy on the screen, required method for game
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 }
 
 // Player Class
-class Player { 
-    constructor() {
-        this.sprite = 'images/char-boy.png' // sprite character
-        this.step = 101;
-        this.jump = 83;
-        this.startx = this.step * 2;
-        this.starty = (this.step * 4) - 20;
-        this.x = this.startx; // x postion
-        this.y = this.starty; // y position
+class Player {
+  constructor() {
+    this.sprite = "images/char-boy.png"; // sprite character
+    this.step = 101;
+    this.jump = 83;
+    this.startx = this.step * 2;
+    this.starty = this.step * 4 - 20;
+    this.x = this.startx; // x postion
+    this.y = this.starty; // y position
+  }
 
-    }
+  update() {
+    // let playerCollision = false;
+    // while (!playerCollision) {
+    //     playerCollision = true;
+    // }
+    // Check for winning the game
+    // Did the player reach the last tile?
+  }
 
-    update() {
-        // let playerCollision = false;
+  render() {
+    // Draw the player sprite on the right x and y coordinate
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 
-        // while (!playerCollision) {
-        //     playerCollision = true;
-        // }
-
-        // Check for winning the game
-            // Did the player reach the last tile?
+  handleInput(input) {
+    switch (input) {
+      case "left":
+        if (this.x > 0) {
+          this.x -= this.step;
         }
-
-    render() {
-        // Draw the player sprite on the right x and y coordinate
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-
-    handleInput(input) {
-        switch(input) {
-            case 'left':
-                if (this.x > 0) {
-                    this.x -= this.step;
-                }
-                break;
-            case 'up':
-                if (this.y > 0) {
-                    this.y -= this.jump;
-                }
-                break;
-            case 'right':
-                if (this.x < 404) {
-                    this.x += this.step;
-                }
-                break;
-            case 'down':
-                if (this.y < this.starty) {
-                    this.y += this.jump;
-                }
-                break;
+        break;
+      case "up":
+        if (this.y > 0) {
+          this.y -= this.jump;
         }
+        break;
+      case "right":
+        if (this.x < 404) {
+          this.x += this.step;
+        }
+        break;
+      case "down":
+        if (this.y < this.starty) {
+          this.y += this.jump;
+        }
+        break;
     }
+  }
 
-    // resetHero()
+  // resetHero()
 }
 
 // Now instantiate your objects.
@@ -93,13 +88,13 @@ let allEnemies = [new Enemy()];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener("keyup", function(e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
